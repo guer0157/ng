c149eb2ce6ea64119fb13e64848ddaf24dbd94a8
Cesar <32390693+guer0157@users.noreply.github.com>
2019-05-29 13:11:53 -0400

    project

---
+++ project/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list/shopping-list.component';
+import { RecipeListComponent } from './recipe -list/recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe -list/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe -list/recipe-detail/recipe-detail.component';
+import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    ShoppingListComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeDetailComponent,
+    ShoppingListEditComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
