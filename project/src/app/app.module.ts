9087b17dc5b970db20619f4515d67e95a559f9ae
Cesar <32390693+guer0157@users.noreply.github.com>
2019-07-30 14:51:24 -0400

    Lecture 56

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list/shopping-list.component';
-import { RecipeListComponent } from './recipe -list/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipe -list/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipe -list/recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipe/recipe-list.component';
+import { RecipeItemComponent } from './recipe/recipe-item/recipe-item.component';
+import { RecipeDetailComponent } from './recipe/recipe-detail/recipe-detail.component';
 import { ShoppingListEditComponent } from './shopping-list/shopping-list-edit/shopping-list-edit.component';
 
 @NgModule({
